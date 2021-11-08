e2a1f432f53e4fd0ec6f065fea573526a7d26198
kranthikk96 <87638903+kranthikk96@users.noreply.github.com>
2021-11-08 12:25:00 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -16,7 +16,7 @@ data "aws_ami" "ubuntu" {
   owners = ["amazon"]
 }
 
-resource "aws_launch_template" "web" {
+resource "Ubuntu Server 20.04 LTS" "web" {
   name_prefix            = "web"
   image_id               = data.ami-083654bd07b5da81d
   instance_type          = var.t2.micro
