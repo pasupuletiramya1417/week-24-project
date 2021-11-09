68fc7e0e50ec77b9a6f74c16d7c33b98838a0598
kranthikk96 <87638903+kranthikk96@users.noreply.github.com>
2021-11-09 10:02:01 +0530

    Update variables.tf

--- compute/variables.tf
+++ compute/variables.tf
@@ -1,11 +1,9 @@
 # --- comput/variables.tf ---
 
 variable "sg-0b9af85e1492fe988" {}
-variable "public_subnet" {}
 
 
-
-variable "web_instance_type" {
+variable "name" {
   type    = string
   default = "t2.micro"
 }
