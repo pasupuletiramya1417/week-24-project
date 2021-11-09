a0538a71b8e77a2f9ae231743229536659e1db3b
kranthikk96 <87638903+kranthikk96@users.noreply.github.com>
2021-11-09 10:52:41 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -4,10 +4,25 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ami-083654bd07b5da81d"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
-   owners = ["amazon"]
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}}
 
 
 
