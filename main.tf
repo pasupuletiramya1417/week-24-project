72e59b1cd4ae6a65cde4e1f911fe845a504ce00e
kranthikk96 <87638903+kranthikk96@users.noreply.github.com>
2021-11-09 09:27:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@
 module "networking" {
   source       = "./networking"
   vpc_cidr     = "10.0.0.0/16"
-  public_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_cidrs = ["10.0.0.0/20"]
 }
 
 module "compute" {
   source        = "./compute"
-  web_sg        = module.networking.web_sg
+  sg        = module.sg
   public_subnet = module.networking.public_subnet
 }
