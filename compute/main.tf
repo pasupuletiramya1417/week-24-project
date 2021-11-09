0215f86295d86e4552727e76df94115b3d2b0453
kranthikk96 <87638903+kranthikk96@users.noreply.github.com>
2021-11-09 09:37:10 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -6,37 +6,7 @@ data "aws_ami" "ubuntu" {
     name   = "name"
     values = ["ami-083654bd07b5da81d"]
   }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["amazon"]
 }
 
-resource "aws_launch_template" "web" {
-  name_prefix            = "web"
-  image_id               = "ami-083654bd07b5da81d"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [var.sg-0b9af85e1492fe988]
-  user_data              = filebase64("install_apache.sh")
 
-  tags = {
-    Name = "web"
-  }
-}
-
-resource "aws_autoscaling_group" "web" {
-  name                = "web"
-  vpc_zone_identifier = tolist(var.public_subnet)
-  min_size            = 2
-  max_size            = 3
-  desired_capacity    = 2
-
-  launch_template {
-    id      = aws_launch_template.web.id
-    version = "$Latest"
-  }
-}
 
