eef7bd0d1e57ec826d03b068c2050b1e570e5a48
kranthikk96 <87638903+kranthikk96@users.noreply.github.com>
2021-11-09 17:39:03 +0530

    Update tfplan.tf

--- tfplan.tf
+++ tfplan.tf
@@ -1,1 +1,33 @@
 #
+tfplan
+├── module() (function)
+│   └── (module namespace)
+│       ├── path ([]string)
+│       ├── data
+│       │   └── TYPE.NAME[NUMBER]
+│       │       ├── applied (map of keys)
+│       │       └── diff
+│       │           └── KEY
+│       │               ├── computed (bool)
+│       │               ├── new (string)
+│       │               └── old (string)
+│       └── resources
+│           └── TYPE.NAME[NUMBER]
+│               ├── applied (map of keys)
+│               ├── destroy (bool)
+│               ├── requires_new (bool)
+│               └── diff
+│                   └── KEY
+│                       ├── computed (bool)
+│                       ├── new (string)
+│                       └── old (string)
+├── module_paths ([][]string)
+├── terraform_version (string)
+├── variables (map of keys)
+│
+├── data (root module alias)
+├── path (root module alias)
+├── resources (root module alias)
+│
+├── config (tfconfig namespace alias)
+└── state (tfstate import alias)
