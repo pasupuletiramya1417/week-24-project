99f553458f0d3d7a4d63e3515a293dc58c01a5ae
pasupuletiramya1417 <79507301+pasupuletiramya1417@users.noreply.github.com>
2021-11-10 14:07:47 +0530

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -23,9 +23,4 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
-resource "aws_cloudformation_stack" "network" {
-  name = "networking-stack"
 
-  parameters = {
-    VPCCidr = "10.0.0.0/16"
-  }
